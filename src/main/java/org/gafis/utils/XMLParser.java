32f98256ba21799a50b9ca8d23fe3e9ad8652eec
shishijie <shishijie@live.com>
2017-09-22 12:08:45 +0800

    修改 soo 打包

---
+++ src/main/java/org/gafis/utils/XMLParser.java
@@ -0,0 +1,60 @@
+package org.gafis.utils;
+
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.*;
+
+/**
+ * Created by yuchen on 2017/8/17.
+ */
+public class XMLParser {
+
+    /**
+     * obj to XML
+     * @param obj
+     * @param load
+     * @return
+     * @throws JAXBException
+     */
+    public static String beanToXml(Object obj,Class<?> load) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(load);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(obj,writer);
+        return writer.toString();
+    }
+
+
+
+    /**
+      * xml文件配置转换为对象
+      * @param file  xml文件file对象
+      * @param load    java对象.Class
+      * @return    java对象
+      * @throws JAXBException
+      * @throws IOException
+     */
+        public static Object xmlToBean(File file,Class<?> load) throws JAXBException, IOException {
+            JAXBContext context = JAXBContext.newInstance(load);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            Object object = unmarshaller.unmarshal(file);
+            return object;
+        }
+
+//    public static void main(String args[]){
+//        try{
+//            String xmlPath = "D:/testConfig.xml";
+//            BufferedWriter bfw = new BufferedWriter(new FileWriter(new File(xmlPath)));
+//            String str = beanToXml(new WebConfig(),WebConfig.class);
+//            bfw.write(str);
+//            bfw.close();
+//        }catch(Exception ex){
+//
+//        }
+//    }
+}
